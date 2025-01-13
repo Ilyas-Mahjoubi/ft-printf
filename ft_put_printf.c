fd113e7a354d303de946de2e5554b1738c5a11e1
Ilyas Mahjoubi <ilmahjou@c2r1p4.42firenze.it>
2025-01-13 15:17:00 +0100

    update

--- ft_put_printf.c
+++ ft_put_printf.c
@@ -6,7 +6,7 @@
 /*   By: ilmahjou <ilmahjou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 22:01:36 by ilmahjou          #+#    #+#             */
-/*   Updated: 2024/12/07 16:31:36 by ilmahjou         ###   ########.fr       */
+/*   Updated: 2024/12/17 15:51:45 by ilmahjou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_print_format(va_list args, const char format)
 	else if (format == 's')
 		print_len += ft_print_str(va_arg(args, char *));
 	else if (format == 'd' || format == 'i')
-		print_len += ft_print_digit((long)va_arg(args, int), 10, 'x');
+		print_len += ft_print_digit(va_arg(args, int), 10, 'x');
 	else if (format == 'x')
 		print_len += ft_print_digit((long)va_arg(args, unsigned int), 16, 'x');
 	else if (format == 'X')
@@ -61,38 +61,18 @@ int	ft_printf(const char *str, ...)
 	va_end(args);
 	return (print_len);
 }
-/* int	ft_printf(const char *str, ...)
+/* #include <stdio.h>
+int main()
 {
-	va_list	args;
-	int		i;
-	int		print_len;
-
-	i = 0;
-	print_len = 0;
-	va_start(args, str);
-	while (str[i])
-	{
-		if (str[i] == '%')
-		{
-			if (str[i + 1])
-			{
-				print_len += ft_print_format(args, str[i + 1]);
-				i++;
-			}
-			else
-			{
-				print_len += ft_print_char('%');
-			}
-		}
-		else
-			print_len += ft_print_char(str[i]);
-		i++;
-	}
-	va_end(args);
-	return (print_len);
-} */
-
-/* int main()
-{
-	ft_printf("hello my name is ;%s\n my age is %d", "ilyas", 20);
+	ft_printf("Character: %c\n", 'A');
+	ft_printf("String: %s\n", "Hello, World!");
+	ft_printf("Decimal: %d\n", 12345);
+	ft_printf("Integer: %i\n", -12345);
+	ft_printf("Hexadecimal (lowercase): %x\n", 255);
+	ft_printf("Hexadecimal (uppercase): %X\n", 255);
+	ft_printf("Percentage: %%\n");
+	ft_printf("Unsigned: %u\n", -255);
+	int x = 42;
+	ft_printf("Pointer: %p\n", &x);
+	return 0;
 } */
